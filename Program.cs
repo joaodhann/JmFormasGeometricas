dda794dee0c0f7fa5240331b8c1ca8369e01b06c
Usuario <Usuario@CPD02-M05>
2024-03-19 11:32:07 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JmFormasGeometricas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escolha uma forma geométrica (Retângulo, Quadrado, Losango e Trapézio: ");
+            string forma = Console.ReadLine().ToUpper();
+
+            Console.WriteLine("Digite a base da forma: ");
+            double bhase = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a altura da forma: ");
+            double altura = double.Parse(Console.ReadLine());
+
+            switch (forma)
+            {
+                case "Quadrado":
+                    double area = bhase * altura;
+                    Console.WriteLine("A área da forma é: " + area);
+                    break;
+                case "Retângulo":
+                    double area2 = bhase * altura;
+                    Console.WriteLine("A área da forma é: " + area2);
+                    break;
+                case "Trapézio":
+                    double area3 = (bhase + bhase) * (altura / 2);
+                    Console.WriteLine("A área da forma é: " + area3);
+                    break;
+                case "Losango":
+                    double area4 = (bhase * altura) / 2;
+                    Console.WriteLine("A área da forma é: " + area4);
+                    break;
+                default:
+                    Console.WriteLine("ERRO");
+                    break;     
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
